d18fdc2103ce93a3c02bf3f51767e7cecb0cf85d
Eric <diego123@Eric.local>
2024-02-06 16:31:51 -0600

    create script list task

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react"
-
-const [title, setTitle] = useState('')
-const [description, setDescription] = useState('')
-
-const handleSubmit = async(e) => {
-  e.preventDefault()
-  const res = await fetch('http://localhost:8000/api/tasks', {
-    method: `POST`,
-    body: JSON.stringify({
-      title,
-      description
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-})
-}
+import axios from "axios"
 
 function TaskForm() {
+
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    // const res = await fetch('http://localhost:8000/api/tasks', {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     title,
+    //     description
+    //   }),
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   }
+
+     const res = await axios.post('http://localhost:8000/api/tasks', {
+      title,
+      description
+    })
+    e.target.reset()
+  }
+
+
   return (
     <div className="flex items-center justify-center h-[calc(100vh-10rem)">
       <form className="bgzinc-950 p-10"
